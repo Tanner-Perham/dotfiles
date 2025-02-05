c1985a55f2ff43082a6a5afc33c14d7c7c9949d1
Tanner Perham <tanner.perham@gmail.com>
2025-02-06 06:07:16 +0800

    Added Colorizer for displaying colours for codes.

--- nvim/.config/nvim/lua/tanner/plugins/init.lua
+++ nvim/.config/nvim/lua/tanner/plugins/init.lua
@@ -2,4 +2,5 @@ return {
 	"nvim-lua/plenary.nvim", -- lua functions that many plugins use
 	"christoomey/vim-tmux-navigator", -- tmux & split window navigation
 	"jinh0/eyeliner.nvim",
+	-- "norcalli/nvim-colorizer.lua",
 }
